be955a4a726af31b28b41a351bf621ff2dfd3435
Isadora Goncalves <isadorag92@yahoo.com.br>
2023-04-25 15:37:01 -0300

    Detalhando dados de Livro

--- src/main/java/study/my/virtual/library/controller/LivrosController.java
+++ src/main/java/study/my/virtual/library/controller/LivrosController.java
@@ -64,5 +64,12 @@ public class LivrosController {
 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity detalhar(@PathVariable Long id) {
+		var livro = repository.getReferenceById(id);
+		
+		return ResponseEntity.ok(new DadosDetalhamentoLivro(livro));
+	}
 
 }
