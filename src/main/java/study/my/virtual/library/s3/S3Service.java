617840efa4a06f33aebe1a306c4e427f259dc077
Isadora Goncalves <isadorag92@yahoo.com.br>
2023-04-27 16:22:29 -0300

    Salvando arquivo de teste no S3

---
+++ src/main/java/study/my/virtual/library/s3/S3Service.java
@@ -0,0 +1,45 @@
+package study.my.virtual.library.s3;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class S3Service {
+	
+	private Logger LOG = LoggerFactory.getLogger(S3Service.class);
+
+	@Autowired
+	private AmazonS3 s3cliente;
+	
+	@Value("${s3.bucket}")
+	private String bucketName;
+	
+	//método responsavel por fazer upload do arquivo que passer pelo caminho para o s3
+	public void uploadFile(String localFilePath) {
+		
+		try {
+		File file = new File(localFilePath);
+		LOG.info("Iniciando upload");
+		s3cliente.putObject(new PutObjectRequest(bucketName, "teste.jpg", file));
+		LOG.info("Finalizado upload");
+		}
+		catch(AmazonServiceException e){
+			LOG.info("AmazonServiceException: " + e.getErrorMessage());
+			LOG.info("Status code: " + e.getErrorCode());
+		}
+		catch(AmazonClientException e) {
+			LOG.info("AmazonClientException: " + e.getMessage());
+		}
+	}
+	
+}
